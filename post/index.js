94dc578ea3132e1882fb2b5cf7d8c1b1f978df26
victorhugoaguilar <victorhugoaguilar@aol.com>
2022-06-11 13:17:47 +0200

    create component react create posts, and add cors policy

--- post/index.js
+++ post/index.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const bodyParser = require('body-parser');
-const { randomBytes} = require('crypto');
+const { randomBytes } = require('crypto');
 const { restart } = require("nodemon");
+const cors = require('cors');
 
 const app = express();
 app.use(bodyParser.json());
+app.use(cors());
 
 const posts = {};
 
@@ -16,12 +18,13 @@ app.post('/posts', (req, res) => {
 
     const id = randomBytes(4).toString('hex');
 
-    const  {title} = req.body;
+    const { title } = req.body;
 
     posts[id] = {
         id, title
     };
 
+    console.log(`add new post -> ${title}`);
     res.status(201).send(posts[id]);
 
 });
