6e8914d01385c8e9c9abf55383fa5d353a67d4d2
victorhugoaguilar <victorhugoaguilar@aol.com>
2022-06-11 13:29:58 +0200

    Add Component List Posts

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,15 @@
 import React from "react";
 
 import PostCreate from "./PostCreate";
- 
+import PostList from "./PostList";
+
 const App = () => {
-  return <div className="container">  
-      <h1>Create Posts</h1>
-      <PostCreate />
+  return <div className="container">
+    <h1>Create Posts</h1>
+    <PostCreate />
+    <hr/>
+    <PostList />
   </div>;
 };
- 
+
 export default App;
